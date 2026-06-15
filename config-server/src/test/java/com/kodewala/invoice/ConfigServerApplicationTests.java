0e69fbd0240535d1f8a240c7211e371ddf9ca23b
kodewala-academy <kodewala.academy@gmail.com>
2026-06-15 16:51:58 +0530

    config server

---
+++ config-server/src/test/java/com/kodewala/invoice/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodewala.invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
