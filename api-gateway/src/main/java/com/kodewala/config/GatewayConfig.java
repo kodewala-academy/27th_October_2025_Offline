8791ea4596490da9944ae7e052ef1b09dd4b6b43
kodewala-academy <kodewala.academy@gmail.com>
2026-06-18 18:12:36 +0530

    enabled rate limiter

---
+++ api-gateway/src/main/java/com/kodewala/config/GatewayConfig.java
@@ -0,0 +1,16 @@
+package com.kodewala.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.just("global");
+    }
+}
