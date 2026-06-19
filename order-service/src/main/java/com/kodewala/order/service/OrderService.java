a34340869dff909c3b7f1cfd43719d3a43359901
kodewala-academy <kodewala.academy@gmail.com>
2026-06-19 15:47:36 +0530

    configured circuit breaker

--- order-service/src/main/java/com/kodewala/order/service/OrderService.java
+++ order-service/src/main/java/com/kodewala/order/service/OrderService.java
@@ -9,6 +9,8 @@ import com.kodewala.order.payment.client.PaymentClient;
 import com.kodewala.order.repository.OrderRepository;
 import com.kodewala.order.request.OrderRequest;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
 public class OrderService {
 
@@ -21,25 +23,43 @@ public class OrderService {
 	@Autowired
 	PaymentClient paymentClient;
 
+	@CircuitBreaker(name = "paymentService", fallbackMethod = "paymentFallback")
 	public void placeOrder(OrderRequest orderRequest) {
+
 		OrderEntity entity = new OrderEntity();
 
 		entity.setItemName(orderRequest.getItemName());
 		entity.setStatus(orderRequest.getStatus());
 
-		// Calling and confirming the payment before placing the order.
+		// Calling payment service 
 		String response = paymentClient.confirmPayment("54364567544", 12000);
-		
-		System.out.println(" Response received from payment service :::::::: " + response);
-		
-		orderRepository.save(entity); // Interacting with database
 
+		System.out.println("Response from payment service : " + response);
+
+		orderRepository.save(entity);
+		
 		/*
 		 * for (int i = 0; i <= 1000; i++) { // Send data to kafka topic..... String
 		 * value = orderRequest.getItemName()+i + " " + orderRequest.getStatus()+i;
 		 * 
 		 * kafkaService.sendMessage("order-confirmed", "key123", value); }
 		 */
+		 
 	}
 
+	public void paymentFallback(OrderRequest orderRequest, Exception ex) {
+
+		System.out.println("Payment service unavailable : " + ex.getMessage());
+
+		// Option 1 : Save order with PAYMENT_PENDING OR do something else
+
+		OrderEntity entity = new OrderEntity();
+
+		entity.setItemName(orderRequest.getItemName());
+		entity.setStatus("PAYMENT_PENDING");
+
+		orderRepository.save(entity);
+		
+		
+	}
 }
